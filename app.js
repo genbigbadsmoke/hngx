59886e0cd198fcad4d4c65c953dfe5878af299d6
genbigbadsmoke <the_boogey@outlook.com>
2023-09-07 13:10:30 +0100

    First Commit

--- app.js
+++ app.js
@@ -6,15 +6,15 @@ var date_time = new Date();
 
 app.get('/intern', (req, res) => {
   return res.json({
-       slack_name: req.query.slack_name,
-       'current_day': 'Thursday',
-       'utc_time': date_time,
-       track: req.query.track,
-       'age': 25,
-       'github_file_url': 'https://github.com/genbigbadsmoke/hngx/blob/main/app.js',
-       'github_repo_url': 'https://github.com/genbigbadsmoke/hngx',
-       'status_code': 200
-   })
+    slack_name: req.query.slack_name,
+    'current_day': 'Thursday',
+    'utc_time': date_time,
+    track: req.query.track,
+    'age': 25,
+    'github_file_url': 'https://github.com/genbigbadsmoke/hngx/blob/main/app.js',
+    'github_repo_url': 'https://github.com/genbigbadsmoke/hngx',
+    'status_code': 200,
+  });
 })
 
 app.listen(3000)
