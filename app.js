9969f61d164e3c972efd8da586922aab90cae452
genbigbadsmoke <the_boogey@outlook.com>
2023-09-07 12:54:01 +0100

    First Commit

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('hello world')
+
+var date_time = new Date();
+
+app.get('/intern', (req, res) => {
+  return res.json({
+       slack_name: req.query.slack_name,
+       'current_day': 'Thursday',
+       'utc_time': date_time,
+       track: req.query.track,
+       'age': 25,
+       'github_file_url': 'https://github.com/genbigbadsmoke/hngx/blob/main/app.js',
+       'github_repo_url': 'https://github.com/genbigbadsmoke/hngx',
+       'status_code': 200
+   })
 })
 
 app.listen(3000)
